1b9f8d75c623cc0113989cbfc86b7f4b688bfd78
aijing-1998 <1438384291@qq.com>
2020-06-19 17:15:14 +0800

    完成登录功能“
    git commit -m 完成登录功能“

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
+import { Form, FormItem, Input, Button, Message } from 'element-ui'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Button)
+Vue.prototype.$message = Message
+
+/* eslint-disable no-new */
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
